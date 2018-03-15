99dfc953c564ee88b5a2a661915688ef902cfc53
celineanne <celine.monigadon@gmx.ch>
2018-03-15 14:32:54 +0100

    register.php checkPSWD()

--- kontakte_mvcgibb/templates/register.htm.php
+++ kontakte_mvcgibb/templates/register.htm.php
@@ -4,4 +4,6 @@
 <input id= "pswd1" type="password" name= "pswd1"></input>
 <input id= "pswd2" type="password" name="pswd2"></input>
 <button type="submit"></button>
+<div id = "fehler"> </div>
+
 </form>
